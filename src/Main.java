6cb97f864e11f7d01f0c50af728c52975890c081
86182 <2431712461@qq.com>
2024-04-03 16:26:53 +0800

    create1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         DormitoryManagementSystem system = new DormitoryManagementSystem();// 添加学生
         system.addStudent(new Student("001", "张三", "1号楼", "101", "1"));
         system.addStudent(new Student("002", "李四", "1号楼", "102", "2"));
-        system.queryStudent("003");
+        system.queryStudent("004");
     }
 }
